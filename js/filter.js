fffb7d9b2335d4b4565132b62da02c18fbdef8ca
Ibrahim Mahdi <Ibrahim.Mahdi@Trustmarque.com>
2026-06-01 16:53:21 +0200

    filtreringsfunktion med receptlista

--- js/filter.js
+++ js/filter.js
@@ -1,1 +1,24 @@
-// filter logik
+function filterRecipes(category) {
+  if (!category || category === "alla") {
+    return recipes;
+  }
+  return recipes.filter(function (recipe) {
+    return recipe.category === category;
+  });
+}
+
+function initFilters(onFilter) {
+  var buttons = document.querySelectorAll(".filter-btn");
+
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      buttons.forEach(function (btn) {
+        btn.classList.remove("active");
+      });
+      button.classList.add("active");
+
+      var category = button.getAttribute("data-category");
+      onFilter(filterRecipes(category));
+    });
+  });
+}
